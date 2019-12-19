ac8bbc6bf950abfb33934af9f4a5450b23450d02
badprson <3243256757@qq.com>
2019-12-19 09:18:10 +0800

    yang

--- 9/resources/views/admin/index.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('admin/layouts.index')
-    @section('content')
-        <fieldset class="layui-elem-field layui-field-title" style="margin-top: 20px;">
-            <legend>后台首页</legend>
-        </fieldset>
-@endsection
