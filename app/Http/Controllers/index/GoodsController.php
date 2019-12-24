060bbbf6f2a46859b0b745ac76798b2c5cbcc194
wen <836127864@qq.com>
2019-12-24 18:42:33 +0800

    wen

--- app/Http/Controllers/index/GoodsController.php
+++ app/Http/Controllers/index/GoodsController.php
@@ -169,15 +169,14 @@ class GoodsController extends Controller
                     $goods_name = $goodsData['goods_name'];
                     $car_num = $v['car_num'];
                     if($car_num >= $goods_num){
-                        echo $car_num;
-                        echo $goods_num;die;
-//                        return json_encode(['code'=>202,'msg'=>$goods_name.'库存不足哦']);
+                        return json_encode(['code'=>202,'msg'=>$goods_name.'库存不足哦','car'=>$car_num,'goods'=>$goods_num]);
                     }else{
                         $carData = Cart::where(['user_id'=>$user_id])
                             ->where(['goods_id'=>$goods_id])
                             ->update(['car_num'=>$car_num]);
                         $goods_num = $goods_num - $car_num;
                         $goodsData = ShopGoods::where(['goods_id'=>$goods_id])->update(['goods_num'=>$goods_num]);
+                        return json_encode(['msg'=>111]);
                     }
                 }
             }
